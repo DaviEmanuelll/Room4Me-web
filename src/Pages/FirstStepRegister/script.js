db73a07a48e5c196327bedc422ab14edc54aa7d4
LucasPaulinoH <lucashsp12@hotmail.com>
2023-04-10 21:09:06 -0300

    feat(Login & Register pages): addition and responsivity adjustments

---
+++ src/Pages/FirstStepRegister/script.js
@@ -0,0 +1,21 @@
+function handleRegister(){
+  let email = document.getElementById("email-input").value;
+  let password = document.getElementById("password-input").value;
+  let emailErrorMsg = document.getElementById("email-error-msg");
+  let passwordErrorMsg = document.getElementById("password-error-msg");
+
+  emailErrorMsg.innerHTML = '';
+  passwordErrorMsg.innerHTML = '';
+
+  if(email == '' || email == undefined){
+    emailErrorMsg.innerHTML = "Preencha o campo de email"
+  } else if(email.indexOf("@") === -1){
+    emailErrorMsg.innerHTML = "O email digitado não é válido"
+  }
+
+  if(password == '' || password == undefined){
+    passwordErrorMsg.innerHTML = "Preencha o campo de senha"
+  } else if(password.length < 8){
+    passwordErrorMsg.innerHTML = "A senha deve conter pelo menos 8 caracteres"
+  }
+}
---
+++ src/Pages/SecondStepRegister/script.js
@@ -0,0 +1,12 @@
+function handleRegister() {
+  let image = document.getElementById("profile-img-input");
+  let name = document.getElementById("name-input").value;
+  let imgErrorMsg = document.getElementById("img-error-msg");
+  let nameErrorMsg = document.getElementById("name-error-msg");
+
+  nameErrorMsg.innerHTML = '';
+  
+  if(name == '' || name == undefined){
+    nameErrorMsg.innerHTML = "Preencha o campo de nome"
+  }
+}
