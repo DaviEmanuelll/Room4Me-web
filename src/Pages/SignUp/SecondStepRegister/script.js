931be302722b31b0d40ffa62fdceabd4ef3b5122
DaviEmanuelll <daviemanuelll04@gmail.com>
2023-04-12 23:14:47 -0300

    Adjusted Pages

--- src/Pages/SignUp/SecondStepRegister/script.js
+++ src/Pages/SignUp/SecondStepRegister/script.js
@@ -1,30 +1,12 @@
-function handleRegister(){
-  let email = document.getElementById("email-input").value;
-  let password = document.getElementById("password-input").value;
-  let passwordConfirmation = document.getElementById("password-confirmation-input").value;
-  let emailErrorMsg = document.getElementById("email-error-msg");
-  let passwordErrorMsg = document.getElementById("password-error-msg");
-  let passwordConfirmationErrorMsg = document.getElementById("password-confirmation-error-msg");
+function handleRegister() {
+  let image = document.getElementById("profile-img-input");
+  let name = document.getElementById("name-input").value;
+  let imgErrorMsg = document.getElementById("img-error-msg");
+  let nameErrorMsg = document.getElementById("name-error-msg");
 
-  emailErrorMsg.innerHTML = '';
-  passwordErrorMsg.innerHTML = '';
-  passwordConfirmationErrorMsg.innerHTML = '';
-
-  if(email == '' || email == undefined){
-    emailErrorMsg.innerHTML = "Preencha o campo de email"
-  } else if(email.indexOf("@") === -1){
-    emailErrorMsg.innerHTML = "O email digitado não é válido"
-  }
-
-  if(password == '' || password == undefined){
-    passwordErrorMsg.innerHTML = "Preencha o campo de senha"
-  } else if(password.length < 8){
-    passwordErrorMsg.innerHTML = "A senha deve conter pelo menos 8 caracteres"
-  }
-
-  if(passwordConfirmation == '' || password == undefined){
-    passwordConfirmationErrorMsg.innerHTML = "Preencha o campo de confirmação da senha"
-  } else if(password.length != password){
-    passwordConfirmationErrorMsg.innerHTML = "As senhas devem ser iguais"
+  nameErrorMsg.innerHTML = '';
+  
+  if(name == '' || name == undefined){
+    nameErrorMsg.innerHTML = "Preencha o campo de nome"
   }
 }
