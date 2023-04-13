3f6b51730f4af5aad01d064fb5f97b9b308edc82
LucasPaulinoH <lucashsp12@hotmail.com>
2023-04-12 23:02:30 -0300

    feat(bedroomQuantity input): positive number verification added

---
+++ src/Pages/HomePage/script.js
@@ -0,0 +1,6 @@
+const bedroomsQuantityInput = document.getElementById('bedroomsQuantity-input');
+bedroomsQuantityInput.addEventListener('input', () => {
+  if (bedroomsQuantityInput.value < 1) {
+    bedroomsQuantityInput.value = 1;
+  }
+});
